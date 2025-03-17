18d7a8b61c4eb3bec8410a8b5a1ed10d9b2a5258
miguel gomez <gomezmiguel088@gmail.com>
2025-03-17 18:04:59 -0500

    actualizacion 17/03/25

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: 'localhost',      // Dirección del servidor de MySQL
   user: 'root',           // Usuario de MySQL
   password: 'mike88', // Contraseña de MySQL
-  database: 'metrasoft'   // Nombre de la base de datos
+  database: 'metrasofft'   // Nombre de la base de datos
 });
 
 // Conectar a la base de datos
