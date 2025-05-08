3ad603ee741f7eaa0e6290b0f01ad68084dae62c
miguel gomez <gomezmiguel088@gmail.com>
2025-05-07 19:04:48 -0500

    se modifica conexiones a la variables de entorno para produccion

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,15 @@
-const mysql = require("mysql2/promise"); //importar al driver MySQL con soporte para promesas
+const mysql = require("mysql2/promise");
 require("dotenv").config(); //carga variables de entorno desde el archivo .env
 
-//crea un pool de conexiones a la base de datos
 const pool = mysql.createPool({
   host: process.env.DB_HOST, //servidor de la base de datos
+  port: process.env.DB_PORT,
   user: process.env.DB_USER, //usuario de la base de datos
   password: process.env.DB_PASSWORD, //contraseña de usuario
   database: process.env.DB_NAME, //nombre de la base de datos
+  ssl: {
+    rejectUnauthorized: true,
+  },
   waitForConnections: true, //mantiene  en cola las solicitudes cuando se alcanza el limite
   connectionLimit: 10, //maximo de conexiones simultaneas
   queueLimit: 0, //limite ilimitado de solicitudes en cola (0 = sin limites )
