5d562b13b6df8a989743e4be39082817486cf1f6
miguel gomez <gomezmiguel088@gmail.com>
2025-05-14 23:40:44 -0500

    2

--- src/server.js
+++ src/server.js
@@ -6,21 +6,26 @@ const path = require("path");
 const cookieParser = require('cookie-parser');
 const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
-const authMiddleware = require('./middlewares/auth');
+
 
 // Inicialización
 const app = express();
+
+app.set('trust proxy', 1);
 
 // Configuración de rutas absolutas
 const publicPath = path.join(__dirname, '../public');
 const htmlPath = path.join(publicPath, 'HTML');
 
 // Middlewares
-app.use(helmet());
+
 app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:3000",
-  credentials: true
+  credentials: true,
+  exposedHeaders: ['Authorization']
 }));
+
+app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -54,7 +59,7 @@ app.use((req, res, next) => {
 app.use(express.static(publicPath));
 
 // Middleware de autenticación
-
+const authMiddleware = require('./middlewares/auth');
 
 // 1. Ruta raíz redirige a login
 app.get('/', redirigirSiAutenticado, (req, res) => {
