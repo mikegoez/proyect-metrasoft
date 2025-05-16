592ae4b7360a3ac3f842a1feb978d43855d6376e
miguel gomez <gomezmiguel088@gmail.com>
2025-05-15 23:31:10 -0500

    rutas 2

--- src/routes/conductoresRoutes.js
+++ src/routes/conductoresRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const conductorController = require('../controllers/conductorController');
 const pool = require("../config/db");
 // Configuración de rutas para el recurso 'conductores'
-router.get('/contar', conductorController.contarConductores);
+
 router.post('/', conductorController.crearConductor);
 router.get('/:numero_documento', conductorController.obtenerConductor);
 router.get('/', conductorController.obtenerConductores);
