7f4bb1f1eaf8f7fc0085916345570661fc367d49
miguel gomez <gomezmiguel088@gmail.com>
2025-05-16 23:38:15 -0500

    verificacion de las rutas

--- src/routes/vehiculosRoutes.js
+++ src/routes/vehiculosRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const vehiculoController = require('../controllers/vehiculoController.js');
+const vehiculoController = require('../controllers/vehiculoController');
 
 // Configuración de rutas RESTful para vehículos
 
 router.post('/', vehiculoController.crearVehiculo);
 router.get("/:placa", vehiculoController.obtenerVehiculo);
-router.get('/', vehiculoController.obtenerVehiculos); 
+router.get('/', vehiculoController.obtenerVehiculo); 
 router.get('/by-id/:id', vehiculoController.obtenerVehiculoPorId);
 router.put("/:placa", vehiculoController.actualizarVehiculo);
 router.delete("/:placa", vehiculoController.eliminarVehiculo);
